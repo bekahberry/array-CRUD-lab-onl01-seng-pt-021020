7d09ae35f6fd8f835493a77768b50049f3ee50eb
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-15 19:09:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array = ["jake", "jess", "angie", "mum"] << "dallas"
+array = ["jake", "jess", "angie", "mum"] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
